72e00b4ba4108a3c41e7d20c12fad8bf230b7dfd
rizkiaulia <39266162+rizkiaulia@users.noreply.github.com>
2020-02-18 04:40:11 +0700

    commit lap daftar

--- admin/lap_pendaftar.php
+++ admin/lap_pendaftar.php
@@ -26,13 +26,26 @@
 							  <i class="fa fa-user-o"></i>
 							</div>
 							<select class="form-control" name="ta" id="filter">
-								<option value="">-Pilih Tahun-</option>
-							 <?php
-							 $ta = mysqli_query($connect, "SELECT * FROM tb_tahunajaran ORDER BY ta_id ASC");
-							 while($data_ta=mysqli_fetch_array($ta)){
-							 ?>
-							  <option value="<?php echo $data_ta['ta_id']; ?>"><?php echo $data_ta['ta_nama']; ?></option>
-							 <?php } ?>
+								<option>--Pilih Tahun--</option>
+                       <?php
+                          $now=date('Y');
+                          	for ($a=$now;$a>=2016;$a--) { 
+                        ?>
+                        <?php if (empty($_GET['ta'])) {
+                        ?>
+                        <option value="<?php echo $a?>"><?php echo $a?> 
+                        <?php
+                          } else {
+                          $thn = $_GET['ta'];
+                            if ($thn == $a) {
+                              $selected = 'selected';
+                            } else {
+                              $selected = '';
+                            }
+                          ?>          
+                          <option value="<?php echo $a?>"<?php echo $selected?>><?php echo $a?>
+                          <?php }?>
+                        <?php }  ?>
 							</select>
 						  </div>
 					  </div>
@@ -44,13 +57,13 @@
 							<div class="input-group-addon">
 							  <i class="fa fa-user-o"></i>
 							</div>
-							<select class="form-control" name="ta" id="filter">
+							<select class="form-control" name="paket" id="filter">
 								<option value="">-Pilih Paket-</option>
 							 <?php
-							 $ta = mysqli_query($connect, "SELECT * FROM tb_tahunajaran ORDER BY ta_id ASC");
-							 while($data_ta=mysqli_fetch_array($ta)){
+							 $paket = mysqli_query($connect, "SELECT * FROM tb_paket ORDER BY paket_id ASC");
+							 while($data_paket=mysqli_fetch_array($paket)){
 							 ?>
-							  <option value="<?php echo $data_ta['ta_id']; ?>"><?php echo $data_ta['ta_nama']; ?></option>
+							  <option value="<?php echo $data_paket['paket_id']; ?>"><?php echo $data_paket['paket_nama']; ?></option>
 							 <?php } ?>
 							</select>
 						  </div>
@@ -79,122 +92,95 @@
 						<div class="form-group row text-left">
                   <div class="col-lg-12">
                      <button class="btn btn-xs btn-primary" name="cari"/><i class="fa fa-search"></i> Tampilkan </button>
-                     <button class="btn btn-xs btn-light" name="reset"/><a href="lap_jadwal.php"><i class="fa fa-delete"></i> Reset Filter </a></button>
+                     <button class="btn btn-xs btn-light" name="reset"/><a href="lap_pendaftar.php"><i class="fa fa-delete"></i> Reset Filter </a></button>
                     
                   </div>
             </div>
 			</form>
 		</div>
   </div>
-              <div class="">
-                <div class="card">
-                  <div class="table-responsive">
-                    <table class="table card-table table-vcenter text-nowrap">
-                      <thead>
-                        <tr>
-						  <th class="w-1">No.</th>
-						  <th>Status Pendaftar</th>
-						  <th>No. Pendaftaran</th>
-						  <th>Tanggal Pendaftaran</th>
-                          <th>Nama</th>
-                          <th>Tempat, Tanggal Lahir</th>
-                          <th>Agama</th>
-                          <th>Jenis Kelamin</th>
-                          <th>Alamat Pendaftar</th>
-                          <th>Nomor HP</th>
-						  <th>Asal Sekolah</th>
-						  <th>Paket Kesetaraan</th>
-						  <th>Kelas Kesetaraan</th>
-						  <th>Putus Sekolah Kelas</th>
-						  <th>Putus Sekolah Semester</th>
-						  <th>Alamat Sekolah</th>
-						  <th>Bertempat Tinggal</th>
-						  <th>Nama Ayah</th>
-						  <th>Nama Ibu</th>
-						  <th>Pekerjaan Ayah</th>
-						  <th>Pekerjaan Ibu</th>
-						  <th>Alamat Ortu</th>
-						  <th>No. HP Ortu/Wali</th>
-						  <th></th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                      	<?php
-	$no=1;
-    if(isset($_GET['filter']) && ! empty($_GET['filter'])){ // Cek apakah user telah memilih filter dan klik tombol tampilkan
-      $filter = $_GET['filter']; // Ambil data filder yang dipilih user
-
-      if($filter == '1'){ // Jika filter nya 1 (per tanggal)
-          $tahun = $_GET['tahun'];
-          echo '<b>Laporan Pendaftaran Tahun '.$_GET['tahun'].'</b><br /><br />';
-          echo '<a target="_blank" href="print_pendaftaran.php?filter=1&tahun='.$_GET['tahun'].'"">Cetak PDF</a><br /><br />';          
-
-          $VarResult = mysqli_query($connect,"SELECT * FROM tb_pendaftar WHERE YEAR(tgl_pendaftaran)='".$_GET['tahun']."' AND status_pendaftar='Diterima'");// Tampilkan data transaksi sesuai th ajaran yang diinput oleh user pada filter
-  }
-
-  }else{ // Jika user tidak mengklik tombol tampilkan
-      echo '<b>Data Pendaftaran</b><br /><br />';
-      echo '<a target="_blank" href="print_pendaftaran.php">Cetak PDF</a><br /><br />';
-
-      $VarResult = mysqli_query($connect,"SELECT * FROM tb_pendaftar WHERE status_pendaftar='Diterima'"); // Tampilkan semua data
-  }
-
-                       //cek data
-                             $varTotal = mysqli_num_rows($VarResult);
-                          // jika data kurang dari 1
-                          if ($varTotal>0) { 
-                      while($h = mysqli_fetch_array($VarResult)){
-
-                    ?>
-                        <tr>
-						  <td><?php echo $no;?></td>
-                          <td><?php echo $h['status_pendaftar'];?></td>
-						  <td><?php echo $h['no_pendaftar'];?></td>
-						  <td><?php echo date("d F Y", strtotime($h['tgl_pendaftaran'])); ?></td>
-						  <td><?php echo $h['nama'];?></td>
-						  <td><?php echo $h['tempat_lhr'] ?> , <?php echo date("d F Y", strtotime($h['tanggal_lhr'])); ?> </td>
-						  <td><?php echo $h['agama'];?></td>
-						  <td><?php echo $h['jenkel'];?></td>
-						  <td><?php echo $h['alamat_domisili'];?></td>
-						  <td><?php echo $h['no_hp'];?></td>
-						  <td><?php echo $h['asal_sekolah'];?></td>
-						  <td><?php echo $h['paket_kesetaraan'];?></td>
-						  <td><?php echo $h['kelas_kesetaraan'];?></td>
-						  <td><?php echo $h['putus_sekolah_kelas'];?></td>
-						  <td><?php echo $h['putus_sekolah_semester'];?></td>
-						  <td><?php echo $h['alamat_sekolah'];?></td>
-						  <td><?php echo $h['bertempat_tinggal'];?></td>
-						  <td><?php echo $h['nama_ayah'];?></td>
-						  <td><?php echo $h['nama_ibu'];?></td>
-						  <td><?php echo $h['pekerjaan_ayah'];?></td>
-						  <td><?php echo $h['pekerjaan_ibu'];?></td>
-						  <td><?php echo $h['alamat_ortu'];?></td>
-						  <td><?php echo $h['no_hp_ortuwali'];?></td>
-			
-                        </tr>
-                      	<?php
-                      $no++; }  
-                    }else{
-                     ?> 
-                     <tr> <!--muncul peringatan bahwa data tidak di temukan-->
-                            <td colspan="9" align="center" style="padding:10px;"> Data Pendaftaran Tidak Ditemukan</td>
-                      </tr>
-                      <?php
-                    } 
-                      ?>
-                      </tbody>
-                    </table>
-                    <script>
-                      require(['datatables', 'jquery'], function(datatable, $) {
-                      	    $('.datatable').DataTable();
-                      	  });
-                    </script>
-                  </div>
-                </div>
-              </div>
-            </div>
+  	<?php
+	    if(isset($_REQUEST['cari'])){
+	?>
+	<div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Laporan Pendaftaran</h3>
+        </div>
+     	<div class="table-responsive">
+			<a class="btn btn-primary" href="cetak_lap_pendaftaran.php?ta=<?php echo $_POST['ta']; ?>&paket=<?php echo $_POST['paket']; ?>&kelas=<?php echo $_POST['kelas'];?>" style="margin-left: 20px; margin-top:10px;">Cetak</a>
+            <table border="0px" style="border-collapse: collapse;" class="table card-table table-vcenter text-nowrap datatable" >
+              <thead>
+              	<tr>
+              		<th class="w-1">No.</th>
+					<th>Status Pendaftar</th>
+					<th>No. Pendaftaran</th>
+					<th>Tanggal Pendaftaran</th>
+                    <th>Nama</th>
+                    <th>Tempat, Tanggal Lahir</th>
+                    <th>Agama</th>
+                    <th>Jenis Kelamin</th>
+                    <th>Alamat Pendaftar</th>
+                    <th>Nomor HP</th>
+					<th>Asal Sekolah</th>
+					<th>Paket Kesetaraan</th>
+					<th>Kelas Kesetaraan</th>
+					<th>Putus Sekolah Kelas</th>
+					<th>Putus Sekolah Semester</th>
+					<th>Nama Ayah</th>
+					<th>Nama Ibu</th>
+					<th>Pekerjaan Ayah</th>
+					<th>Pekerjaan Ibu</th>
+					<th>Alamat Ortu</th>
+					<th>No. HP Ortu/Wali</th>
+              	</tr>
+              </thead>
+               <tbody>
+             <?php 
+                $no=1;
+                $kelas = @$_POST['kelas'];
+				$ta = @$_POST['ta'];
+				$paket = @$_POST['paket'];
+			$pendaftar = mysqli_query($connect, "SELECT * FROM tb_pendaftar WHERE status_pendaftar='Diterima'");
+			while($data_pendaftar = mysqli_fetch_array($pendaftar)){
+              ?>
+                <tr>
+                  <td align="center"><span class="text-muted"><?php echo $no;?></span></td>
+                  <td align="center"><?php echo $data_pendaftar['status_pendaftar'] ?></td>
+                  <td align="center"><?php echo $data_pendaftar['no_pendaftar'] ?></td>
+                  <td align="center"><?php echo $data_pendaftar['tgl_pendaftaran'] ?></td>
+                  <td align="justify"><?php echo $data_pendaftar['nama'] ?></td>
+                  <td align="justify"><?php echo $data_pendaftar['tempat_lhr']?> , <?php echo date("d F Y", strtotime($data_pendaftar['tanggal_lhr'])); ?></td>
+                  <td align="justify"><?php echo $data_pendaftar['agama'] ?></td>
+                  <td align="justify"><?php echo $data_pendaftar['jenkel'] ?></td>
+                  <td align="justify"><?php echo $data_pendaftar['alamat_domisili'] ?></td>
+                  <td align="justify"><?php echo $data_pendaftar['no_hp'] ?></td>
+                  <td align="justify"><?php echo $data_pendaftar['asal_sekolah'] ?></td>
+                  <td align="justify"><?php echo $data_pendaftar['paket_kesetaraan'] ?></td>
+                  <td align="justify"><?php echo $data_pendaftar['kelas_kesetaraan'] ?></td>
+                  <td align="justify"><?php echo $data_pendaftar['putus_sekolah_kelas'] ?></td>
+                  <td align="justify"><?php echo $data_pendaftar['putus_sekolah_semester'] ?></td>
+                  <td align="justify"><?php echo $data_pendaftar['nama_ayah'] ?></td>
+                  <td align="justify"><?php echo $data_pendaftar['nama_ibu'] ?></td>
+                  <td align="justify"><?php echo $data_pendaftar['pekerjaan_ayah'] ?></td>
+                  <td align="justify"><?php echo $data_pendaftar['pekerjaan_ibu'] ?></td>
+                  <td align="justify"><?php echo $data_pendaftar['alamat_ortu'] ?></td>
+                  <td align="justify"><?php echo $data_pendaftar['no_hp_ortuwali'] ?></td>
+                  
+                  
+                </tr>
+          <?php $no++;}} ?>
+              </tbody>
+        <script>
+              require(['datatables', 'jquery'], function(datatable, $) {
+                    $('.datatable').DataTable();
+                  });
+            </script>
+           
           </div>
-          <!-- Modal Popup Dosen -->
+        </div>
 		
+      </div>
+    </div>
+  </div>
   </body>
 </html>
